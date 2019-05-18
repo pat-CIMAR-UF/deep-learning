13426419b6440c0ab69ffdb6ba0d26c60c298d9f
Yiqun-Pat-Wang <36198135+pat-CIMAR-UF@users.noreply.github.com>
2019-05-18 17:47:46 -0400

    Add files via upload

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 3000
+iterations = 2000
 learning_rate = 0.7
 hidden_nodes = 20
 output_nodes = 1
